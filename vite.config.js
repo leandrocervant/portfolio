76dcf018e93af35f0d0f1759970cfbff2fc58144
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2024-11-28 13:05:36 -0300

    update Vite base path for correct deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: "./portfolio",
+  base: "/portfolio",
   plugins: [react(), svgr()],
 });
