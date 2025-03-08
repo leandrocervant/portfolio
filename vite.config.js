ed9e0cebfc12f90077062ab0ce0f5ed386e86e30
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2025-03-08 13:26:31 -0300

    feat: add CSS module support with scoped class name generation in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import path from "node:path";
 
 export default defineConfig({
   base: "/portfolio",
+  css: {
+    modules: {
+      generateScopedName(className, file) {
+        const parts = [];
+
+        const filePath = file.split("?")[0];
+        const fileName = path.basename(filePath, ".module.css");
+        const foldersPath = path.dirname(filePath).split(path.sep);
+        const folderName = foldersPath[foldersPath.length - 1];
+
+        const componentName = fileName !== "root" ? fileName : folderName;
+
+        parts.push(componentName);
+
+        if (className !== "root") {
+          parts.push(className);
+        }
+
+        return parts.join("__");
+      },
+    },
+  },
   plugins: [react(), svgr()],
 });
